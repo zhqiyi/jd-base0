49c105ffa6b420453d550373f4dd147c285df00e
ZFeng3242 <771849136@qq.com>
2021-06-08 17:45:31 +0800

    update

--- git_pull.sh
+++ git_pull.sh
@@ -23,7 +23,7 @@ ContentNewTask=${ShellDir}/new_task
 ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
 isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
-ShellURL=https://hub.fastgit.org/DX3242/jd-base
+ShellURL=https://hub.fastgit.org/DX3242/jd-base0
 ScriptsURL=https://gitee.com/ZFeng3242/JD-haoyangmao
 
 ## 更新crontab，gitee服务器同一时间限制5个链接，因此每个人更新代码必须错开时间，每次执行git_pull随机生成。
